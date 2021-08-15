86e225693e1b7a9439fbdb04c738caee3dca01e3
ID-lmntr1x <torel@protonmail.com>
2021-08-14 22:21:32 -0500

    first commit

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+USER_CONFIG_DIR="$HOME/.config/"
+
+# delete config files
+rm -rf "$USER_CONFIG_DIR"rofi
+rm -rf "$USER_CONFIG_DIR"polybar
+rm -rf "$USER_CONFIG_DIR"sxhkd
+rm -rf "$USER_CONFIG_DIR"bspwm
+rm -rf "$USER_CONFIG_DIR"picom
+rm -rf "$USER_CONFIG_DIR"bin
+# install config files in user config dir & fonts
+cp -r .fonts rofi polybar sxhkd bspwm picom  "$USER_CONFIG_DIR"
+# reload sxhkd
+pkill -USR1 -x sxhkd
+# reload bspwm
+bspc wm -r 
