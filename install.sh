d47be553825e4b28bdc5bec91a1b1593f5960956
ID-lmntr1x <88756782+ID-lmntr1x@users.noreply.github.com>
2021-08-17 00:50:50 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,8 +9,9 @@ rm -rf "$USER_CONFIG_DIR"sxhkd
 rm -rf "$USER_CONFIG_DIR"bspwm
 rm -rf "$USER_CONFIG_DIR"picom
 rm -rf "$USER_CONFIG_DIR"bin
+rm -rf "$USER_CONFIG_DIR"screenshot
 # install config files in user config dir & fonts
-cp -r .fonts rofi polybar sxhkd bspwm picom  "$USER_CONFIG_DIR"
+cp -r .fonts rofi polybar sxhkd bspwm picom bin screenshot "$USER_CONFIG_DIR"
 # reload sxhkd
 pkill -USR1 -x sxhkd
 # reload bspwm
